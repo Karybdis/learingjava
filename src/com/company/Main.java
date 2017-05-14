571c1ecceb630e65e3ea0ffd414cc9e42ee5bda5
sunsc <584156664@qq.com>
2017-05-14 19:48:25 +0800

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("请输入对应的序号");
+
+        Scanner scanf=new Scanner(System.in);
+
+    }
+}
