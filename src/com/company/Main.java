d76deaf8c1eeb0bf632ad8b737c0d8c5ac4ca176
sunsc <584156664@qq.com>
2017-05-14 20:02:37 +0800

    second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,20 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("请输入对应的序号");
+        System.out.println("1.学生成绩查询")；
+        System.out.println("2.退出系统");
 
-        Scanner scanf=new Scanner(System.in);
+        Scanner scanf = new Scanner(System.in);
+        int num = scanf.nextInt();
+        switch (num)
+        {
 
+            case 1:
+
+            case 2:
+                System.exit(0);
+
+
+        }
     }
 }
